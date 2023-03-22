62a86a3b784559dde7174bd51ed48be03c2260f5
nelly amar <nelly.am@hotmail.fr>
2023-03-23 00:55:28 +0200

    sign up player

--- get-to-100/src/component/sign_up.jsx
+++ get-to-100/src/component/sign_up.jsx
@@ -1,55 +1,74 @@
 import React, {Component} from 'react';
 import '../styles.css'
+
+
 class Sign_up extends Component {
-    // constructor(props){
-    //     super(props)
-        state = { 
-            name:'', 
-            firtsname:'',
+    constructor(props){
+        super(props)
+        this.state = { 
+            lastname:'', 
+            firstname:'',
             mail:''
          };
-    // this.SubmitPlayer=this.SubmitPlayer(this);
-    // }
+    this.handleLastNameChange=this.handleLastNameChange.bind(this);
+    this.handleFirstNameChange=this.handleFirstNameChange.bind(this);
+    this.handleMailChange=this.handleMailChange.bind(this);
+    this.SubmitPlayer=this.SubmitPlayer.bind(this);
+   }
    
-     handleNameChange=event=>{
-        console.log(event)
-        this.setState({name: event.target.value});
-     }; 
-     handleFirstNameChange=event=>{
+     handleLastNameChange(event){
+        this.setState({lastname: event.target.value});
+     } 
+     handleFirstNameChange(event){
         this.setState({firstname: event.target.value});
-     }; 
-     handleMailChange=event=>{
+     } 
+     handleMailChange(event){
         this.setState({mail: event.target.value});
-     }; 
+     }
 
-    //  SubmitPlayer(){
-    //     var player={
-    //         name:this.state.name,
-    //         firstname:this.state.firstname,
-    //         mail:this.state.mail
-    //     }
-    //     localStorage.setItem("player1", JSON.stringify(player));
-    //  }  onSubmit={this.SubmitPlayer}
+     SubmitPlayer(){
+        var player={
+            lastname:this.state.lastname,
+            firstname:this.state.firstname,
+            mail:this.state.mail,
+            list_game:null
+        }
+        let list_of_player;
+        list_of_player=JSON.parse(localStorage.getItem("list_of_player"));
+        if(list_of_player!=null)
+        { 
+            localStorage.removeItem("list_of_player")
+            list_of_player.push(player);
+            localStorage.setItem("list_of_player", JSON.stringify(list_of_player));
+        }else{
+            var new_list_player=[]
+            new_list_player.push(player)
+            localStorage.setItem("list_of_player", JSON.stringify(new_list_player))
+        }
+        localStorage.setItem(this.state.mail, JSON.stringify(player));
+       
+        
+     }  
     render() { 
         const {name, firstname, mail}=this.state;
         return (
             <div>
                 <p  className='title_game'>Welcome to Get to 100 Game</p>
-                <form className='form_registration'> 
+                <form className='form_registration' onSubmit={this.SubmitPlayer}> 
                     <label className='title_label'>
-                        Name:
+                        Last Name:
                     </label><br />
-                    <input className='input_field' type="text" value={name} onChange={this.handleNameChange} required></input>
+                    <input className='input_field' type="text" value={this.state.lastname} onChange={this.handleLastNameChange} required></input>
                     <br />
                     <label  className='title_label'>
                         First Name:
                     </label><br />
-                    <input className='input_field' type="text" value={firstname} onChange={this.handleFirstNameChange} required></input>
+                    <input className='input_field' type="text" value={this.state.firstname} onChange={this.handleFirstNameChange} required></input>
                     <br />
                     <label  className='title_label'>
                         Mail:
                     </label><br />
-                    <input className='input_field' type="email" value={mail} onChange={this.handleMailChange} required></input><br />
+                    <input className='input_field' type="email" value={this.state.mail} onChange={this.handleMailChange} required></input><br />
                     <input className='submit_button' type="submit" value="Add Player"></input>
                 </form>
             </div>
